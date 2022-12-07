49cef2326973f8244cddc34c5abded8e32552883
Teeboi09 <111147825+Teeboi09@users.noreply.github.com>
2022-12-07 11:40:08 -0800

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
-
-    if (a < b):
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+if __name__ == '__main__':
+    from sys import argv
+nargs = len(argv)
+if nargs == 1:
+    print("{:d} arguments.".format(nargs - 1))
+elif nargs > 1:
+    message = "argument" if (nargs == 2) else "arguments"
+    print("{:d} {:s}:".format(nargs - 1, message))
+    for i in range(1, nargs):
+        print("{:d}: {:s}".format(i, argv[i]))
